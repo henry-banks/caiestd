e09fe472247aef9c370695ee67e7f3aad6c6b259
kelsey.davis <kelsey.davis@students.aie.edu.au>
2016-08-31 11:03:24 -0700

    setup source file

---
+++ CodeTrials/kdivconq.cpp
@@ -0,0 +1,46 @@
+#include "caiestd.h"
+#include <cstdio>
+
+
+
+int divideAndConquer(int nums[], size_t numSize)
+{
+
+
+}
+
+
+
+
+/*
+int divideAndConquer(int in1, int in2)
+{
+	if (in1 > in2)
+	{
+		printf(" %d ", in1);
+		return in1;
+	}
+	else if (in1 < in2)
+	{
+		printf(" %d ", in2);
+		return in2;
+	}
+	else if (in1 == in2)
+	{
+		printf("you can't enter the same number!\n");
+
+	}
+
+
+
+	return 0;
+}*/
+
+/*
+int scanNum()
+{
+	scanf_s("%d %d", &in1, &in2);
+
+	return divideAndConquer(in1, in2);
+}
+*/
